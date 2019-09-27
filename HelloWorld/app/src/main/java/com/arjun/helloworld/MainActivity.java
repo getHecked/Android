00675514ecf1eca10b00fe2dcab052874f1cd9ff
getHecked <arjn.nair@gmail.com>
2019-09-27 14:40:48 +0300

    added openBrowser action

--- HelloWorld/app/src/main/java/com/arjun/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/arjun/helloworld/MainActivity.java
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private Button childButton;
     private Button cameraButton;
+    private Button browserButton;
     private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
         childButton = findViewById(R.id.childButton);
         childButton.setBackgroundColor(getColor(R.color.colorAccent));
         cameraButton = findViewById(R.id.cameaButton);
+        browserButton = findViewById(R.id.openBrowser);
+        editText = findViewById(R.id.editText);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +64,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        browserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = editText.getText().toString();
+                if(Patterns.WEB_URL.matcher(url).matches()){
+                    Uri webpage = Uri.parse(url);
+
+                    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                        webpage = Uri.parse("https://" + url);
+                    }
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
+            }else{
+                    Toast.makeText(MainActivity.this, "Invalid URL", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 
 
     @Override
